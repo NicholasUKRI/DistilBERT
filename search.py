88c1e414c1a256c1a7a7986bb3a9c8f69d97b55e
Nicholas Hooper <55379993+NicholasUKRI@users.noreply.github.com>
2022-04-25 13:27:27 +0100

    Update search.py
    deleted redundant functions

--- search.py
+++ search.py
@@ -51,18 +51,6 @@ def cosine_similarity(v, M):
     return (M.matmul(v.T)).squeeze().div(dM * dv)
 
 
-def L2_similarity(v, M):
-    """
-    Calculate the L2 / Euclidean distance between all rows in M and v.
-    The L2 norm is calculated as the square root of the sum of the squared vector values.
-    
-    :param v: torch.tensor of size (1, n_latent)
-    :param M: torch.tensor of size (n_documents, n_latent)
-    :return: torch.tensor of size (n_documents,)
-    """
-    return -torch.cdist(M, v).squeeze()
-
-
 def return_ranked(query, tokenizer, model, M):
     """
     Embed a `query` using `model` and `tokenizer`, and return the
@@ -80,30 +68,6 @@ def return_ranked(query, tokenizer, model, M):
     sims = sims[rankings].tolist()
     ranks = rankings.tolist()
     return list(zip(ranks, sims))
-
-
-def return_ranked_by_sentence(query, tokenizer, model, indices, M):
-    """
-    Embed a `query` using `model` and `tokenizer`, and return the
-    indices of sentence embeddings `M` sorted by most to least similar.
-
-    :param query: str of query
-    :param tokenizer: HuggingFace tokenizer instance
-    :param model: HuggingFace model instance
-    :param indices: torch.tensor of size (n_sentences)
-    :param M: torch.tensor of size (n_documents, n_latent)
-    :return: a list of ints of length `n_documents`
-    """
-    q = embed(query, tokenizer, model)
-    sims = cosine_similarity(q, M)
-    rankings = torch.argsort(sims, descending=True)
-    doc_rankings = indices[rankings].numpy()
-    _, first_doc_rankings = np.unique(doc_rankings, return_index=True)
-    final_doc_rankings = doc_rankings[first_doc_rankings]
-    matching_sims = sims[doc_rankings][first_doc_rankings]
-    ranks = final_doc_rankings.tolist()
-    matching_sims = matching_sims.tolist()
-    return list(zip(ranks, matching_sims))
 
 
 # Loads the vector embeddings
