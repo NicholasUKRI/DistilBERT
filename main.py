f63e82d889a938a6c06405e7158951b1ff7c95c9
Nicholas Hooper <55379993+NicholasUKRI@users.noreply.github.com>
2022-04-29 12:52:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,35 @@ import torch
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
+# Authenticate App once
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+
+if gauth.credentials is None:
+    # Authenticate if they're not there
+
+    # This is what solved the issues:
+    gauth.GetFlow()
+    gauth.flow.params.update({'access_type': 'offline'})
+    gauth.flow.params.update({'approval_prompt': 'force'})
+
+    gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
 drive = GoogleDrive(gauth)
+
+
 
 @st.cache
 def get_metadata():
